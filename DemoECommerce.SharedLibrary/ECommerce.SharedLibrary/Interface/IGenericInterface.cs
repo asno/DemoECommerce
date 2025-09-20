78dac80a9be285a402de3c6781419d5521c243b7
schnoebelen <alexandre.schnoebelen@gmail.com>
2025-09-20 22:37:40 +0700

    Creating Response
    Creating Log Exception
    
    Creating Generic Interface

---
+++ DemoECommerce.SharedLibrary/ECommerce.SharedLibrary/Interface/IGenericInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using ECommerce.SharedLibrary.Responses;
+
+
+namespace ECommerce.SharedLibrary.Interface
+{
+    public interface IGenericInterface<T> where T : class
+    {
+        Task<Response> CreateAsync(T entity);
+        Task<Response> UpdateAsync(T entity);
+        Task<Response> DeleteAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate);
+    }
+}
