d03ae8ab3bb9406cbd34ff82c0ee1133d3f027f6
schnoebelen <alexandre.schnoebelen@gmail.com>
2025-09-21 18:04:44 +0700

    Creating Global Exception Handler Middleware
    Creating Middleware to Redirect All Requests to Gateway

---
+++ DemoECommerce.SharedLibrary/ECommerce.SharedLibrary/Middleware/ApiGatewayEnforcement.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.SharedLibrary.Middleware
+{
+    public class ApiGatewayEnforcement (RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var signedHeader = context.Response.Headers["api-gateway"];
+
+            if (signedHeader.FirstOrDefault() is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("Service unavailable.");
+                return;
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}
---
+++ DemoECommerce.SharedLibrary/ECommerce.SharedLibrary/Middleware/GlobalException.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+using ECommerce.SharedLibrary.Logs;
+
+namespace ECommerce.SharedLibrary.Middleware
+{
+    public class GlobalException(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string message = "Internal server error occured.";
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            string title = "Error";
+
+            try
+            {
+                await next(context);
+
+                if (context.Response.StatusCode == StatusCodes.Status429TooManyRequests)
+                {
+                    title = "Warning";
+                    message = "Too many requests.";
+                    statusCode = (int)HttpStatusCode.TooManyRequests;
+                }
+                if (context.Response.StatusCode == StatusCodes.Status401Unauthorized)
+                {
+                    title = "Alert";
+                    message = "You are not authorized to access.";
+                    statusCode = (int)HttpStatusCode.Unauthorized;
+                }
+                if (context.Response.StatusCode == StatusCodes.Status403Forbidden)
+                {
+                    title = "Out of access";
+                    message = "You are not allowed to access.";
+                    statusCode = (int)HttpStatusCode.Forbidden;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogException.LogExceptions(ex);
+
+                if (ex is TaskCanceledException || ex is TimeoutException)
+                {
+                    title = "Time Out";
+                    message = "The request timed out.";
+                    statusCode = (int)HttpStatusCode.RequestTimeout;
+                }
+            }
+            finally
+            {
+                await ModifyHeader(context, title, message, statusCode);
+            }
+            return;
+        }
+
+        private static async Task ModifyHeader(HttpContext context, string title, string message, int statusCode)
+        {
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize(new ProblemDetails()
+            {
+                Detail = message,
+                Title = title,
+                Status = statusCode
+            }), CancellationToken.None);
+            return;
+        }
+    }
+}
