0dadbe0cfba30f2ce8401990b71e61bee710b903
schnoebelen <alexandre.schnoebelen@gmail.com>
2025-09-21 18:04:44 +0700

    Creating Global Exception Handler Middleware
    Creating Middleware to Redirect All Requests to Gateway

---
+++ DemoECommerce.SharedLibrary/ECommerce.SharedLibrary/Middleware/ApiGatewayEnforcement.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ECommerce.SharedLibrary.Middleware
+{
+    public class ApiGatewayEnforcement (RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var signedHeader = context.Response.Headers["api-gateway"];
+
+            if (signedHeader.FirstOrDefault() is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("Service unavailable.");
+                return;
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}
