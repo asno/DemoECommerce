178137260d4d9949503d151e6cb6b8189e86e092
schnoebelen <alexandre.schnoebelen@gmail.com>
2025-09-21 22:44:27 +0700

    Creating JWT Authentication Scheme
    Creating Dependency Injection Container

---
+++ DemoECommerce.SharedLibrary/ECommerce.SharedLibrary/DependencyInjection/SharedServiceContainer.cs
@@ -0,0 +1,44 @@
+﻿using ECommerce.SharedLibrary.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System.Runtime.CompilerServices;
+
+namespace ECommerce.SharedLibrary.DependencyInjection
+{
+    public static class SharedServiceContainer
+    {
+        public static IServiceCollection AddSharedServices<TContext>
+            (this IServiceCollection services, IConfiguration config, string fileName) where TContext : DbContext
+        {
+            services.AddDbContext<TContext>(option => option.UseSqlServer(
+                config
+                .GetConnectionString("eCommerceConnection"), sqlServerOption =>
+                sqlServerOption.EnableRetryOnFailure()));
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(path: $"{fileName}-.text",
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {message:lj}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            JWTAuthenticationScheme.AddJWTAuthenticationScheme(services, config);
+            return services;
+        }
+
+        public static IApplicationBuilder UseSharedPolicies(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<GlobalException>();
+
+            app.UseMiddleware<ApiGatewayEnforcement>();
+
+            return app;
+        }
+    }
+}
