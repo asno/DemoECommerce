b4f6c03de5515eeeed619af9077b99287d042110
schnoebelen <alexandre.schnoebelen@gmail.com>
2025-09-22 23:49:06 +0700

    Create Product API Project
    Create Product Entity and DTO

---
+++ DemoECommerce.ProductAPI/ProductAPI.Application/DTO/Conversions/ProductConversion.cs
@@ -0,0 +1,37 @@
+﻿using ProductAPI.Domain.Entities;
+
+namespace ProductAPI.Application.DTO.Conversions
+{
+    public static class ProductConversion
+    {
+        public static Product ToEntity(ProductDTO productDTO) => new()
+        {
+            Id = productDTO.Id,
+            Name = productDTO.Name,
+            Quantity = productDTO.Quantity,
+            Price = productDTO.Price
+        };
+
+        public static (ProductDTO?, IEnumerable<ProductDTO>?) FromEntity(Product product, IEnumerable<Product>? products)
+        {
+            if (product != null && products == null)
+            {
+                var singleProduct = new ProductDTO
+                    (
+                        Id: product.Id,
+                        Name: product.Name!,
+                        Quantity: product.Quantity,
+                        Price: product.Price
+                    );
+                return (singleProduct, null);
+            }
+            if (product == null && products != null)
+            {
+                var listOfProducts = products.Select(p => new ProductDTO(p.Id, p.Name!, p.Quantity, p.Price)).ToList();
+                return (null, listOfProducts);
+            }
+
+            return (null, null);
+        }
+    }
+}
