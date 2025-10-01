eb277196d216d6efd0ebe52e893d7b0821c9f99b
schnoebelen <alexandre.schnoebelen@gmail.com>
2025-10-01 19:46:57 +0700

    Create dependency injection and service container

---
+++ DemoECommerce.AuthenticationAPI/AuthenticationAPI.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,29 @@
+﻿using AuthenticationAPI.Application.Interface;
+using AuthenticationAPI.Infrastructure.Data;
+using AuthenticationAPI.Infrastructure.Repositories;
+using ECommerce.SharedLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthenticationAPI.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            SharedServiceContainer.AddSharedServices<AuthenticationDbContext>(services, config, config["MySerilog:FileName"]!);
+
+            services.AddScoped<IUser, UserRepository>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            SharedServiceContainer.UseSharedPolicies(app);
+
+            return app;
+        }
+    }
+}
---
+++ DemoECommerce.AuthenticationAPI/AuthenticationAPI.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,52 @@
+﻿using AuthenticationAPI.Application.DTO;
+using AuthenticationAPI.Application.Interface;
+using ECommerce.SharedLibrary.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationAPI.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController(IUser userInterface) : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<Response>> Register(AppUserDTO appUserDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await userInterface.Register(appUserDTO);
+            return result.Flag ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<Response>> Login(LoginDTO loginDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await userInterface.Login(loginDTO);
+            return result.Flag ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<AppUserWithoutPasswordDTO>> GetUser(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest("Invalid argument passed.");
+            }
+
+            var user = await userInterface.GetUser(id);
+            if (user is null)
+            {
+                return NotFound("User not found.");
+            }
+
+            return user.Id > 0 ? Ok(user) : BadRequest(user);
+        }
+    }
+}
